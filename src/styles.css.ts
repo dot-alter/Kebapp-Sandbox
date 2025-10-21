c2915c6c1ff2af545cf1ff4884c34c29cc0b4f32
LordMaster <renaissancelordandmaster@gmail.com>
2025-10-20 21:28:31 -0400

    Change background style property

--- src/styles.css.ts
+++ src/styles.css.ts
@@ -1,7 +1,7 @@
 import { globalStyle } from "@vanilla-extract/css";
 
 globalStyle("body", {
-    margin: 0,
-    padding: 0,
-    background: "#FFF",
+    margin:     0,
+    padding:    0,
+    background: "#1e1e1f",
 });
